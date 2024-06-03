a2ec693a5466f515bc1d1433b6595a2f81e6d1c2
Ray-Shakex <bshakex@gmail.com>
2024-06-03 12:50:41 +0200

    scroller

--- js/app.js
+++ js/app.js
@@ -170,3 +170,27 @@ ioItem.forEach(item => {
     );
     itemObserver.observe(item);
 });
+
+const scrollers = document.querySelectorAll(".scroller");
+
+if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+}
+
+function addAnimation() {
+    scrollers.forEach((scroller) => {
+        scroller.setAttribute("data-animated", true);
+
+        const scrollerInner = scroller.querySelector('.scroller__inner');
+        const scrollerContent = Array.from(scrollerInner.children);
+        console.log(scrollerContent);
+
+        scrollerContent.forEach(item => {
+            const duplicatedItem = item.cloneNode(true);
+            // PREVENT DUPLICATED ITEMS FROM BEING READ BY SCREEN READERS
+            duplicatedItem.setAttribute('aria-hidden', true);
+            console.log(duplicatedItem);
+            scrollerInner.appendChild(duplicatedItem);
+        })
+    });
+}
